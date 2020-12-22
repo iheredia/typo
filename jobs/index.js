adb1522370565a375f973f66b3c2e0b9edc25d06
Ignacio Heredia <ignacio.nh@gmail.com>
2020-12-22 16:45:25 -0300

    Add simple request to google fonts api

---
+++ jobs/index.js
@@ -0,0 +1,10 @@
+const axios = require('axios');
+const { apiKey } = require('../credentials.json');
+
+const getPopularFonts = async () => {
+  const googleFontsAPIUrl = 'https://www.googleapis.com/webfonts/v1/webfonts';
+  const params = { sort: 'popularity', key: apiKey };
+
+  const { data: { items } } = await axios.get(googleFontsAPIUrl, { params })
+  return items
+}
